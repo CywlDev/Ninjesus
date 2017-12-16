5bb6c95e3cdfe9bcaea85cdee10998dbf4d1515d
Peter Waysocher <peter.waysocher@gmail.com>
2017-12-17 00:25:43 +0100

    Dmg Recognition

--- Assets/Scripts/General/HealthScript.cs
+++ Assets/Scripts/General/HealthScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Lifetime;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour {
@@ -14,6 +15,14 @@ public class HealthScript : MonoBehaviour {
 	/// </summary>
 	public bool isEnemy = true;
 
+	
+	
+	private void DmgColor()
+	{
+		var obj = this.GetComponentInParent(typeof(PlayerScript));
+		obj.GetComponent<SpriteRenderer>().color = new Color(1f,1f, 1f);
+	}
+	
 	/// <summary>
 	/// Inflicts damage and check if the object should be destroyed
 	/// </summary>
@@ -23,8 +32,12 @@ public class HealthScript : MonoBehaviour {
 		hp -= damageCount;
 
 		var obj = this.GetComponentInParent(typeof(PlayerScript));
+		if (obj != null)
+		{
+			obj.GetComponent<SpriteRenderer>().color = new Color(1f,0.30196078f, 0.30196078f);
+			Invoke("DmgColor", 0.2f);
 
-		
+		}
 		
 		if (hp <= 0)
 		{
